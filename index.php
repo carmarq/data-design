67fb72a8e877fd756f1c4ce5224735fc05a9cbb4
Carlos Marquez <carl.marq95@gmail.com>
2018-04-13 17:43:36 -0600

    started my persona listed technology used listed attitudes

--- index.php
+++ index.php
@@ -1,10 +1,29 @@
-!DOCTYPE html>
-<html>
-	<head> </head>
+<!DOCTYPE html>
+<html lang="en">
+	<meta charset="utf-8">
+	<head>
+		<title>Data Design Project</title>
+	</head>
 	<body>
-		<p>Vince</p>
-		<p>33 years old</p>
-		<p></p>
+		<h1>Persona		</h1>
+		<ul>
+		<li>Vince</li>
+		<li>33 years old</li>
+		<li>Male</li>
+		</ul>
+		<br>
+		<h2>Technology Used</h2>
+		<ul>
+			<li>Laptop: 2011 MacBook Pro (OS X Lion) </li>
+			<li>Phone: LGV30 (Android 7.1.2)</li>
+		</ul>
+		<br>
+		<h2>Attitudes</h2>
+		<ul>
+			<li>Is excited to use web applications</li>
+			<li>Feels confident about using web applications </li>
+			<li></li>
+		</ul>
 	</body>
 </html>
 
