2ff8341fc588f5d3b978a8a8227c27f67904b8f5
Carlos Marquez <carl.marq95@gmail.com>
2018-04-14 08:11:46 -0600

    started my persona listed technology used listed attitudes finished persona

--- index.php
+++ index.php
@@ -5,25 +5,42 @@
 		<title>Data Design Project</title>
 	</head>
 	<body>
-		<h1>Persona		</h1>
-		<ul>
-		<li>Vince</li>
-		<li>33 years old</li>
-		<li>Male</li>
-		</ul>
+	<h1>Persona		</h1>
+			<ul>
+			<li>Vince</li>
+			<li>33 years old</li>
+			<li>Male</li>
+			</ul>
 		<br>
-		<h2>Technology Used</h2>
-		<ul>
+	<h2>Technology Used</h2>
+			<ul>
 			<li>Laptop: 2011 MacBook Pro (OS X Lion) </li>
 			<li>Phone: LGV30 (Android 7.1.2)</li>
-		</ul>
+			</ul>
 		<br>
-		<h2>Attitudes</h2>
-		<ul>
-			<li>Is excited to use web applications</li>
-			<li>Feels confident about using web applications </li>
-			<li></li>
-		</ul>
+	<h2>Attitudes</h2>
+			<ul>
+			<li>Is excited to use web applications.</li>
+			<li>Feels confident about using web applications.</li>
+			<li>Generally pretty content.</li>
+			</ul>
+		<br>
+	<h2>Goals</h2>
+			<ul>
+			<li>Vince is looking for the better quality (highest thc/cbd content) products.</li>
+			<li>He also hopes to find what he's looking for quickly.</li>
+			</ul>
+		<br>
+	<h2>Frustrations</h2>
+			<ul>
+			<li>Better available products, and a wider selection at good prices.</li>
+			<li>What has precluded him from reaching his goal before was not being able to see current stock in the first place.</li>
+			</ul>
+		<br>
+		<br>
+	<h1>User Story</h1>
+	<h1>Use Case/Interaction Flow</h1>
+	<h1>Conceptual Model</h1>
 	</body>
 </html>
 
