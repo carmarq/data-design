6a0c13888f50165d0a5e00ac3dfd1b202c3a83c4
Carlos Marquez <carl.marq95@gmail.com>
2018-04-23 17:57:34 -0600

    added insert, delete and update statements

---
+++ sql/oml-assignment.sql
@@ -0,0 +1,22 @@
+/**
+* inserts this Profile into mysql
+*
+*
+ **/
+INSERT INTO profile(profileId, profileEmail, profileFavorites, profileHash, profileLocation, profileUsername)
+VALUES(UNHEX(REPLACE("aebdaeff-75ae-4039-b520-b506578efa3f", "dmscus@gmail.com", "Raspberry Kush", "564283", "Albuquerque, NM", "dmscus");
+
+INSERT INTO profile(profileId, profileEmail, profileFavorites, profileHash, profileLocation, profileUsername)
+VALUES(UNHEX(REPLACE("9fa0132a-f8a1-4377-9341-63e8ec033795", "sucsmd@gmail.com", "Trainwreck", "453256", "Tucson, AZ", "sucsmd");
+
+DELETE FROM profileFavorites WHERE USER = 'Ice Cream Cake'
+DELETE FROM profileFavorites where USER = 'Strawberry Blonde'
+
+UPDATE profile
+SET profileFavorites = 'Reba'
+WHERE profileEmail = 'dmscus@gmail.com'
+
+UPDATE profile
+SET profileLocation = 'Roswell, NM'
+Where profileUsername = 'sucsmd'
+
